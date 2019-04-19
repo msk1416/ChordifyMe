2677c191a8948c62d0580fc740e282e5fb20d9ef
Sergi Mascaró <msk1416@gmail.com>
2019-04-19 20:47:34 +0200

    Add functional context menu

--- background.js
+++ background.js
@@ -1,53 +1,28 @@
-var newUrl;
-var newTabId;
+var clickHandler = function(info){
+    let url =  info.linkUrl.split("://")[1];
+    var encodedYtUrl = encodeURIComponent(url);
+    var finalUrl = "https://chordify.net/search/www.youtube.com/" + encodedYtUrl + "?from=chordifyme";
+    chrome.tabs.create({url: finalUrl});
+};
+var menuItem = {
+    id:"chordifyTheSong",
+    title: "Chordify this song",
+    contexts: ["link"],
+    targetUrlPatterns: ["*://*.youtube.com/watch*"],
+    onclick: clickHandler
+};
 
-chrome.browserAction.onClicked.addListener(function(activeTab) {
-    // let activeTab = tabs[0];
-    openNewTab(activeTab);
-})
+chrome.contextMenus.create(menuItem);
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    let script = 'alert("' + changeInfo.status + ');';
-    if (tab.url.indexOf(newUrl) != -1 || true) {
-        chrome.tabs.executeScript(null, {code: script})
+    if (tab.url.indexOf('youtube.com/watch') != -1){
+        chrome.pageAction.show(tabId);
+    } else {
+        chrome.pageAction.hide(tabId);
     }
-})
-
-function openNewTab(activeTab) {
-    alert('icon clicked');
-    console.log(activeTab.url);
-    chrome.tabs.executeScript(activeTab.id, {code:"console.log('Testing background scripts...');"});
-    let url =  activeTab.url.split("://")[1];
-    var encodedYtUrl = encodeURIComponent(url);
-    // document.getElementById("message").textContent = encodedUrl;
-    var finalUrl = "https://chordify.net/search/www.youtube.com/" + encodedYtUrl;
-    newUrl = finalUrl;
-    //var win = window.open(finalUrl, "_blank");
-    chrome.tabs.create({url: finalUrl, active: false}, function(tab) {
-        newTabId = tab.id;
-        setTimeout(checkPageLoaded, 1000, tab.id);
-    });
-    chrome.tabs.executeScript(activeTab.id, {code:"console.log('New tab id is " + newTabId +"');"});
-    chrome.tabs.executeScript({
-        // file: "clickSearchResult.js"
-        code: "console.log('Testing testing...');"
-    }, function(results){
-        //alert(results);
-    });
-}
-
- function checkPageLoaded(tabid) {
-    
-    chrome.tabs.query({id: tabid }, function (tab) {
-        console.log("Query returned " + tab.length + " results.")
-        for(var i = 0; i < tab.length; i++){ 
-            console.log(tab[i]);
-            // if(tab[i].title =! undefined) {
-            //     chrome.tabs.executeScript(tab[i].id, {code:'console.log("Hey!!");'});
-            // }
-            // else{
-            //     setTimeout(checkPageLoaded, 1000) 
-            // }
-        }
-    });
- }
+    if (tab.url.indexOf('chordify.net/search') != -1 && tab.url.indexOf('?from=chordifyme') != -1 && changeInfo.status == 'complete') {
+        chrome.tabs.executeScript(null, { 
+            file: "clickSearchResult.js"
+        });
+    }
+});
